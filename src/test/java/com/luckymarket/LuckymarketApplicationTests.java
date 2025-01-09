f95d815190eaae2c623415661c977962aa707ab8
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-01-10 01:29:20 +0900

    config: 프로젝트 생성 및 폴더 구조 구성

---
+++ src/test/java/com/luckymarket/LuckymarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckymarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckymarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
