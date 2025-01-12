858bf884354bee5abee0ed94a9e343bb958b7a18
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-01-12 18:04:01 +0900

    feat: 비밀번호 검증 로직 구현
    - 비밀번호 검증 (8자 이상, 대소문자 포함, 특수문자 포함 등)

--- src/main/java/com/luckymarket/user/service/SignupServiceImpl.java
+++ src/main/java/com/luckymarket/user/service/SignupServiceImpl.java
@@ -16,6 +16,30 @@ public class SignupServiceImpl implements SignupService {
 
     @Override
     public void signup(Member member) {
+        validatePassword(member.getPassword());
 
+        userRepository.save(member);
+    }
+
+    private static void validatePassword(String password) {
+        if (password == null || password.trim().isEmpty()) {
+            throw new IllegalArgumentException("비밀번호는 필수 항목입니다.");
+        }
+
+        if (password.length() < 8) {
+            throw new IllegalArgumentException("비밀번호는 최소 8자 이상이어야 합니다.");
+        }
+
+        if (!password.matches(".*[A-Z].*")) {
+            throw new IllegalArgumentException("비밀번호는 최소 1개의 대문자를 포함해야 합니다.");
+        }
+
+        if (!password.matches(".*[a-z].*")) {
+            throw new IllegalArgumentException("비밀번호는 최소 1개의 소문자를 포함해야 합니다.");
+        }
+
+        if (!password.matches(".*[!@#$%^&*(),.?\":{}|<>].*")) {
+            throw new IllegalArgumentException("비밀번호는 최소 1개의 특수문자를 포함해야 합니다.");
+        }
     }
 }
