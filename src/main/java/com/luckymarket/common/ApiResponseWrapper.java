e55afef40597d58ac1497915f6f120bd05b099c2
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-01-20 18:28:17 +0900

    docs: ApiResponseWrapper에 Swagger 문서화 주석 추가

--- src/main/java/com/luckymarket/common/ApiResponseWrapper.java
+++ src/main/java/com/luckymarket/common/ApiResponseWrapper.java
@@ -1,5 +1,6 @@
 package com.luckymarket.common;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,9 +10,13 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "공통 API 응답 모델")
 public class ApiResponseWrapper<T> {
+    @Schema(description = "응답 메시지")
     private String message;
+    @Schema(description = "HTTP 상태 코드")
     private int status;
+    @Schema(description = "응답 데이터")
     private T data;
 
     public static <T> ApiResponseWrapper<T> success(String message, T data){
