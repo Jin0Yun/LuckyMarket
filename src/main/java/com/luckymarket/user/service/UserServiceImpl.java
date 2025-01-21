3307edf811fa04dd3ccb07a95941a87052752bf8
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-01-21 20:46:22 +0900

    feat: 유저 이름 변경 기능 구현

--- src/main/java/com/luckymarket/user/service/UserServiceImpl.java
+++ src/main/java/com/luckymarket/user/service/UserServiceImpl.java
@@ -2,6 +2,8 @@ package com.luckymarket.user.service;
 
 import com.luckymarket.user.domain.Member;
 import com.luckymarket.user.dto.NameUpdateDto;
+import com.luckymarket.user.exception.UserErrorCode;
+import com.luckymarket.user.exception.UserException;
 import com.luckymarket.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +19,9 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Member updateName(Long userId, NameUpdateDto nameDto) {
-        return Member.builder().build();
+        Member member = userRepository.findById(userId)
+                .orElseThrow(() -> new UserException(UserErrorCode.NAME_BLANK));
+        member.setUsername(nameDto.getNewName());
+        return userRepository.save(member);
     }
 }
