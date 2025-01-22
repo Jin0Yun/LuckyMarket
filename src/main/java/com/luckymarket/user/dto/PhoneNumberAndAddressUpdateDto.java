648dd0bec4b838e713c7b42875c2498d2329d115
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-01-22 15:18:42 +0900

    feat: PhoneNumberAndAddressUpdateDto 클래스 추가

---
+++ src/main/java/com/luckymarket/user/dto/PhoneNumberAndAddressUpdateDto.java
@@ -0,0 +1,11 @@
+package com.luckymarket.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PhoneNumberAndAddressUpdateDto {
+    private String phoneNumber;
+    private String address;
+}
