6576af168fbf006545bb4678939365bb32fa8b26
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-02-03 14:42:01 +0900

    feat: ValidationRule 인터페이스 기반의 MemberValidationService 구현
    - 회원가입 및 사용자 정보 수정 시 검증 로직 통함

---
+++ src/main/java/com/luckymarket/user/service/MemberValidationService.java
@@ -0,0 +1,52 @@
+package com.luckymarket.user.service;
+
+import com.luckymarket.user.domain.Member;
+import com.luckymarket.user.validator.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberValidationService {
+    private final ValidationRule<String> phoneNumberValidationRule;
+    private final ValidationRule<String> emailValidationRule;
+    private final ValidationRule<String> passwordValidationRule;
+    private final ValidationRule<String> addressValidationRule;
+
+    @Autowired
+    public MemberValidationService(
+            PhoneNumberValidationRule phoneNumberValidator,
+            EmailValidationRule emailValidator,
+            PasswordValidationRule passwordValidator,
+            AddressValidationRule addressValidator
+    ) {
+        this.phoneNumberValidationRule = phoneNumberValidator;
+        this.emailValidationRule = emailValidator;
+        this.passwordValidationRule = passwordValidator;
+        this.addressValidationRule = addressValidator;
+    }
+
+    public void validateSignupFields(Member member) {
+        validateEmail(member.getEmail());
+        validatePassword(member.getPassword());
+    }
+
+    public void validateEmail(String email) {
+        emailValidationRule.validate(email);
+    }
+
+    public void validatePassword(String password) {
+        passwordValidationRule.validate(password);
+    }
+
+    public void validatePhoneNumber(String phoneNumber) {
+        if (phoneNumber != null) {
+            phoneNumberValidationRule.validate(phoneNumber);
+        }
+    }
+
+    public void validateAddress(String address) {
+        if (address != null) {
+            addressValidationRule.validate(address);
+        }
+    }
+}
