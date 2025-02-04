9254bb02ece2ea2ede4adb54422132e9493aa3d9
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-02-04 12:04:22 +0900

    feat: MemberMapper 인터페이스 추가 및 구현

--- src/main/java/com/luckymarket/user/mapper/MemberMapper.java
+++ src/main/java/com/luckymarket/user/mapper/MemberMapper.java
@@ -1,18 +1,10 @@
 package com.luckymarket.user.mapper;
 
 import com.luckymarket.user.domain.Member;
-import com.luckymarket.user.domain.Role;
-import com.luckymarket.user.domain.Status;
+import com.luckymarket.user.dto.MemberResponseDto;
 import com.luckymarket.user.dto.SignupRequestDto;
 
-public class MemberMapper {
-    public static Member toEntity(SignupRequestDto dto) {
-        return Member.builder()
-                .email(dto.getEmail())
-                .password(dto.getPassword())
-                .username(dto.getUsername())
-                .role(Role.USER)
-                .status(Status.ACTIVE)
-                .build();
-    }
+public interface MemberMapper {
+    MemberResponseDto toMemberResponseDto(Member member);
+    Member toEntity(SignupRequestDto dto);
 }
