2f4e27e30c70d73184f72920e6c0ea0bd5126dfb
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-02-07 04:31:41 +0900

    refactor: RedisService 메서드 수정

--- src/main/java/com/luckymarket/auth/service/redis/RedisService.java
+++ src/main/java/com/luckymarket/auth/service/redis/RedisService.java
@@ -5,10 +5,8 @@ import java.util.Optional;
 public interface RedisService {
     void saveRefreshToken(Long userId, String refreshToken, long expiration);
     Optional<String> getRefreshToken(Long userId);
-    void deleteRefreshToken(Long userId);
-    void deleteBlacklistToken(String token);
+    void removeRefreshToken(Long userId);
     void addToBlacklist(String token, long expiration);
     boolean isBlacklisted(String token);
-    boolean isKeyExist(String key);
-    boolean isUserLoggedIn(Long userId);
+    void removeFromBlacklist(String accessToken);
 }
