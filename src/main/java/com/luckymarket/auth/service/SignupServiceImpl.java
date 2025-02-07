25562b5286bdb4d1d4b54a7f67af6cda95d677e7
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-02-07 18:22:42 +0900

    refactor: MemberValidationServiceImpl -> AuthValidationService로 변경하여 검증 로직 분리

--- src/main/java/com/luckymarket/auth/service/SignupServiceImpl.java
+++ src/main/java/com/luckymarket/auth/service/SignupServiceImpl.java
@@ -1,5 +1,6 @@
 package com.luckymarket.auth.service;
 
+import com.luckymarket.auth.validator.AuthValidationService;
 import com.luckymarket.user.domain.exception.UserErrorCode;
 import com.luckymarket.user.domain.exception.UserException;
 import com.luckymarket.user.domain.model.Member;
@@ -7,7 +8,6 @@ import com.luckymarket.user.domain.model.Member;
 import com.luckymarket.user.usecase.dto.SignupRequestDto;
 import com.luckymarket.user.adapter.mapper.MemberMapper;
 import com.luckymarket.user.domain.repository.UserRepository;
-import com.luckymarket.user.usecase.service.MemberValidationService;
 import com.luckymarket.user.usecase.service.PasswordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,19 +16,19 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class SignupServiceImpl implements SignupService {
     private final UserRepository userRepository;
-    private final MemberValidationService memberValidationService;
+    private final AuthValidationService authValidationService;
     private final PasswordService passwordService;
     private final MemberMapper memberMapper;
 
     @Autowired
     public SignupServiceImpl(
             UserRepository userRepository,
-            MemberValidationService memberValidator,
+            AuthValidationService authValidationService,
             PasswordService passwordService,
             MemberMapper memberMapper
     ) {
         this.userRepository = userRepository;
-        this.memberValidationService = memberValidator;
+        this.authValidationService = authValidationService;
         this.passwordService = passwordService;
         this.memberMapper = memberMapper;
     }
@@ -36,12 +36,12 @@ public class SignupServiceImpl implements SignupService {
     @Override
     @Transactional
     public void signup(SignupRequestDto signupRequestDto) {
-        memberValidationService.validateEmail(signupRequestDto.getEmail());
+        authValidationService.validateEmail(signupRequestDto.getEmail());
         if (userRepository.existsByEmail(signupRequestDto.getEmail())) {
             throw new UserException(UserErrorCode.EMAIL_ALREADY_USED);
         }
 
-        memberValidationService.validatePassword(signupRequestDto.getPassword());
+        authValidationService.validatePassword(signupRequestDto.getPassword());
 
         signupRequestDto.setPassword(passwordService.encodePassword(signupRequestDto.getPassword()));
         Member member = memberMapper.toEntity(signupRequestDto);
