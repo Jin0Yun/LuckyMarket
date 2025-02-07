40bbc354d8981aafab291d2ee2257f2ded17381a
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-02-07 17:10:38 +0900

    feat: 로그인 서비스에서 비밀번호 검증 및 로그인 상태 처리 추가

--- src/main/java/com/luckymarket/auth/service/login/LoginService.java
+++ src/main/java/com/luckymarket/auth/service/login/LoginService.java
@@ -2,15 +2,15 @@ package com.luckymarket.auth.service.login;
 
 import com.luckymarket.auth.dto.LoginRequestDto;
 import com.luckymarket.auth.dto.LoginResponseDto;
-import com.luckymarket.auth.dto.TokenResponseDto;
 import com.luckymarket.auth.exception.AuthErrorCode;
 import com.luckymarket.auth.exception.AuthException;
+import com.luckymarket.auth.exception.RedisException;
 import com.luckymarket.auth.security.JwtTokenProvider;
 import com.luckymarket.auth.service.redis.RedisService;
 import com.luckymarket.user.domain.model.Member;
 import com.luckymarket.user.domain.model.Status;
 import com.luckymarket.user.domain.repository.UserRepository;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import com.luckymarket.user.usecase.service.PasswordService;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -18,14 +18,20 @@ import java.time.LocalDateTime;
 @Service
 public class LoginService {
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final PasswordService passwordService;
     private final JwtTokenProvider jwtTokenProvider;
     private final RedisService redisService;
     private final LoginValidator loginValidator;
 
-    public LoginService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtTokenProvider jwtTokenProvider, RedisService redisService, LoginValidator loginValidator) {
+    public LoginService(
+            UserRepository userRepository,
+            PasswordService passwordService,
+            JwtTokenProvider jwtTokenProvider,
+            RedisService redisService,
+            LoginValidator loginValidator
+    ) {
         this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
+        this.passwordService = passwordService;
         this.jwtTokenProvider = jwtTokenProvider;
         this.redisService = redisService;
         this.loginValidator = loginValidator;
@@ -44,11 +50,10 @@ public class LoginService {
             throw new AuthException(AuthErrorCode.USER_ALREADY_DELETED);
         }
 
-        if (!passwordEncoder.matches(loginRequestDto.getPassword(), member.getPassword())) {
-            throw new AuthException(AuthErrorCode.PASSWORD_MISMATCH);
-        }
-
-        if (redisService.isUserLoggedIn(member.getId())) {
+        passwordService.matches(loginRequestDto.getPassword(), member.getPassword());
+        try {
+            redisService.markUserAsLoggedIn(member.getId());
+        } catch (RedisException e) {
             throw new AuthException(AuthErrorCode.ALREADY_LOGGED_IN_OTHER_DEVICE);
         }
 
