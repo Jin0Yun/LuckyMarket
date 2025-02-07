989bc26b5705c5900f5c6e7b7dc8a4f87ddb3150
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-02-07 17:11:09 +0900

    feat: RedisErrorCode에 새로운 에러 코드 추가

--- src/main/java/com/luckymarket/auth/exception/RedisErrorCode.java
+++ src/main/java/com/luckymarket/auth/exception/RedisErrorCode.java
@@ -10,7 +10,11 @@ public enum RedisErrorCode {
     BLACKLIST_TOKEN_NOT_FOUND("블랙리스트 토큰을 찾을 수 없습니다."),
     BLACKLIST_TOKEN_DELETE_FAILED("블랙리스트 토큰 삭제에 실패했습니다."),
 
-    KEY_EXIST_CHECK_FAILED("키 존재 여부 확인 중 오류가 발생했습니다.");
+    KEY_EXIST_CHECK_FAILED("키 존재 여부 확인 중 오류가 발생했습니다."),
+
+    ALREADY_LOGGED_IN_OTHER_DEVICE("다른 디바이스에서 이미 로그인 되어 있습니다."),
+    KEY_SAVE_FAILED("로그인 상태 저장에 실패했습니다."),
+    KEY_DELETE_FAILED("로그인 상태 삭제에 실패했습니다.");
 
     private final String message;
 
