a0c68c6b84896e155b8458870e45addb240a71e0
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-02-07 17:10:00 +0900

    feat: PasswordService에 matches 메서드 추가
    - PasswordService에 비밀번호 일치 여부 확인을 위한 matches 메서드 추가

--- src/main/java/com/luckymarket/user/usecase/service/PasswordService.java
+++ src/main/java/com/luckymarket/user/usecase/service/PasswordService.java
@@ -2,4 +2,5 @@ package com.luckymarket.user.usecase.service;
 
 public interface PasswordService {
     String encodePassword(String password);
+    void matches(String password, String encodedPassword);
 }
