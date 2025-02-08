797f8f563a0fff85ba26b4647592d8acc11daa08
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-02-08 18:53:00 +0900

    test: 전화번호 검증 로직 단위 테스트 추가

---
+++ src/test/java/com/luckymarket/user/usecase/validator/PhoneNumberValidationRuleTest.java
@@ -0,0 +1,51 @@
+package com.luckymarket.user.usecase.validator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.luckymarket.user.domain.exception.UserErrorCode;
+import com.luckymarket.user.domain.exception.UserException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PhoneNumberValidationRuleTest {
+    private PhoneNumberValidationRule phoneNumberValidationRule;
+
+    @BeforeEach
+    void setUp() {
+        phoneNumberValidationRule = new PhoneNumberValidationRule();
+    }
+
+    @DisplayName("전화번호가 빈 문자열일 경우 예외를 던진다.")
+    @Test
+    void validate_ShouldThrowException_WhenPhoneNumberIsBlank() {
+        // given
+        String phoneNumber = "";
+
+        // when & then
+        UserException exception = assertThrows(UserException.class, () -> phoneNumberValidationRule.validate(phoneNumber));
+        assertThat(exception.getMessage()).isEqualTo(UserErrorCode.PHONE_NUMBER_BLANK.getMessage());
+    }
+
+    @DisplayName("전화번호가 잘못된 형식일 경우 예외를 던진다.")
+    @Test
+    void validate_ShouldThrowException_WhenPhoneNumberIsInvalidFormat() {
+        // given
+        String phoneNumber = "12345abc";
+
+        // when & then
+        UserException exception = assertThrows(UserException.class, () -> phoneNumberValidationRule.validate(phoneNumber));
+        assertThat(exception.getMessage()).isEqualTo(UserErrorCode.INVALID_PHONE_NUMBER_FORMAT.getMessage());
+    }
+
+    @DisplayName("전화번호가 유효한 형식일 경우 예외가 발생하지 않는다.")
+    @Test
+    void validate_ShouldNotThrowException_WhenPhoneNumberIsValid() {
+        // given
+        String phoneNumber = "+1234567890";
+
+        // when & then
+        assertDoesNotThrow(() -> phoneNumberValidationRule.validate(phoneNumber));
+    }
+}
