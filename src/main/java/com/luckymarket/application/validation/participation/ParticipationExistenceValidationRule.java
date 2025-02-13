39f1007481185eddb6479088bda269d472d44056
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-02-13 22:07:33 +0900

    refactor: ValidationRule 인터페이스 -> EntityRetrievalRuleWithTwoParams로 변경

--- src/main/java/com/luckymarket/application/validation/participation/ParticipationExistenceValidationRule.java
+++ src/main/java/com/luckymarket/application/validation/participation/ParticipationExistenceValidationRule.java
@@ -1,8 +1,7 @@
 package com.luckymarket.application.validation.participation;
 
 import com.luckymarket.adapter.out.persistence.participation.ParticipationRepository;
-import com.luckymarket.application.dto.participation.ParticipationRequest;
-import com.luckymarket.application.validation.ValidationRule;
+import com.luckymarket.application.validation.EntityRetrievalRuleWithTwoParams;
 import com.luckymarket.domain.entity.participation.Participation;
 import com.luckymarket.domain.exception.persistence.ParticipationErrorCode;
 import com.luckymarket.domain.exception.persistence.ParticipationException;
@@ -11,17 +10,18 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class ParticipationExistenceValidationRule implements ValidationRule<ParticipationRequest> {
+public class ParticipationExistenceValidationRule implements EntityRetrievalRuleWithTwoParams<Participation, Long, Long> {
     private final ParticipationRepository participationRepository;
 
     @Override
-    public void validate(ParticipationRequest request) {
-        if (participationRepository.existsByProductIdAndMemberId(request.getProduct().getId(), request.getUserId())) {
+    public void validate(Participation request) {
+        if (participationRepository.existsByProductIdAndMemberId(request.getProduct().getId(), request.getMember().getId())) {
             throw new ParticipationException(ParticipationErrorCode.ALREADY_PARTICIPATED);
         }
     }
 
-    public Participation getParticipation(Long productId, Long userId) {
+    @Override
+    public Participation getEntity(Long productId, Long userId) {
         return participationRepository.findByProductIdAndMemberId(productId, userId)
                 .orElseThrow(() -> new ParticipationException(ParticipationErrorCode.NOT_PARTICIPATED));
     }
